8409aa56322f7228d06c4eacdbe0b1e6667f3008
Nithin K <19286034+nithin-vijayan@users.noreply.github.com>
2020-04-30 21:50:33 +0530

    Added tag api endpoints

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,14 @@
+from core import models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+
+def sample_user(email='user@app.com', password='userpass'):
+    """Create sample user"""
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password
+    )
 
 
 class ModelTestCase(TestCase):
@@ -38,3 +47,11 @@ class ModelTestCase(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test tag string repr"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
